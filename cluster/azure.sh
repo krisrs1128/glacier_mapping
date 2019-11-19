3ff27e7b58b94b6f4ef8435b7deee1f63f7aee3c
Kris Sankaran <sankaran.kris@gmail.com>
2019-11-19 13:55:15 -0500

    make sure rasterio is installed

--- cluster/azure.sh
+++ cluster/azure.sh
@@ -11,5 +11,5 @@ sudo apt -y install python3-pip
 sudo apt -y install build-essential libssl-dev libffi-dev python3-dev
 sudo apt -y install nano vim ipython3
 
-pip3 install numpy pandas wandb Pillow addict pyyaml
+pip3 install numpy pandas wandb Pillow addict pyyaml rasterio
 pip3 install --no-cache-dir torch torchvision matplotlib seaborn

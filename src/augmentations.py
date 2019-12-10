b38e12adb364bbf324b17a1c4b5928a6d33577bf
Shimaa <shimaa.baraka@gmail.com>
2019-12-09 22:46:58 -0500

    return same variables

--- src/augmentations.py
+++ src/augmentations.py
@@ -19,11 +19,9 @@ def rotate(img, mask, rot=(-10, 10), p=0.5):
         center = int(w / 2), int(h / 2)
 
         rot_mat = cv2.getRotationMatrix2D(center, angle, 1)
-        rotated_img = cv2.warpAffine(img, rot_mat, (w, h))
-        rotated_mask = cv2.warpAffine(mask, rot_mat, (w, h),
-                                      flags=cv2.INTER_NEAREST)
-        return rotated_img, rotated_mask
-
+        img = cv2.warpAffine(img, rot_mat, (w, h))
+        mask = cv2.warpAffine(mask, rot_mat, (w, h), flags=cv2.INTER_NEAREST)
+        
     return img, mask
 
 def flip(img, mask, direction, percent=0.5):
@@ -34,5 +32,4 @@ def flip(img, mask, direction, percent=0.5):
         img = cv2.flip(img, direction)
         mask = cv2.flip(mask, direction)
 
-        return img, mask
     return img, mask

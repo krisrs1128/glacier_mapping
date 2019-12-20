8e9722b46ecd6ea724c066e5e36eb1ca3032abac
Shimaa <shimaa.baraka@gmail.com>
2019-12-20 18:03:40 -0500

    change log of preprocessing

--- cluster/sbatch_preprocessing.sh
+++ cluster/sbatch_preprocessing.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:1                     # Ask for 1 GPU
 #SBATCH --mem=32G                        # Ask for 32 GB of RAM
 #SBATCH --time=3:00:00                   # The job will run for 3 hours
-#SBATCH -o /scratch/<user>/slurm-%j.out  # Write the log in $SCRATCH
+#SBATCH -o $SCRATCH/logs/glaciers_preprocessing-%j.out  # Write the log in $SCRATCH
 
 # Copy and unzip the raw data to the compute node
 cp $SCRATCH/raw_glaciers_data.zip $SLURM_TMPDIR

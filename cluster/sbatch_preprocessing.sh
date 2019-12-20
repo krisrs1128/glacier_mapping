af28683c520f7437f12478cdb1076e2eaf3d8fbd
Shimaa <shimaa.baraka@gmail.com>
2019-12-20 18:41:51 -0500

    change log of preprocessing

--- cluster/sbatch_preprocessing.sh
+++ cluster/sbatch_preprocessing.sh
@@ -3,10 +3,15 @@
 #SBATCH --cpus-per-task=6                # Ask for 6 CPUs
 #SBATCH --gres=gpu:1                     # Ask for 1 GPU
 #SBATCH --mem=32G                        # Ask for 32 GB of RAM
-#SBATCH --time=3:00:00                   # The job will run for 3 hours
-#SBATCH -o $SCRATCH/logs/glaciers_preprocessing-%j.out  # Write the log in $SCRATCH
+#SBATCH --time=5:00:00                   # The job will run for 5 hours
+#SBATCH -o /scratch/shimaa/logs/glaciers_preprocessing-%j.out  # Write the log in $SCRATCH
+#SBATCH -e /scratch/shimaa/logs/glaciers_preprocessing-%j.err  # Write the err in $SCRATCH
+
+# change the log directories based on your space
 
 # Copy and unzip the raw data to the compute node
+# this assumes you have the raw_data on your scratch space,
+# if it doesn't, you can copy it from /scratch/shimaa (everyone has read access)
 cp $SCRATCH/raw_glaciers_data.zip $SLURM_TMPDIR
 unzip $SLURM_TMPDIR/raw_glaciers_data.zip -d $SLURM_TMPDIR
 

d45e8d596444940c6f9a84c020ffefde6120e014
Shimaa <shimaa.baraka@gmail.com>
2019-12-23 15:23:53 -0500

    fix sbatch errors

--- cluster/sbatch_preprocessing.sh
+++ cluster/sbatch_preprocessing.sh
@@ -14,18 +14,18 @@
 # this assumes you have the raw_data on your scratch space,
 # if it doesn't, you can copy it from /scratch/shimaa (everyone has read access)
 cp $SCRATCH/raw_glaciers_data.zip $SLURM_TMPDIR
-unzip $SLURM_TMPDIR/raw_glaciers_data.zip -d $SLURM_TMPDIR
+unzip $SLURM_TMPDIR/data/raw_glaciers_data.zip -d $SLURM_TMPDIR
 
 module load singularity/3.4
 cd $HOME/glacier_mapping
 # TODO: put all in one command
 singularity exec --bind $SLURM_TMPDIR /scratch/sankarak/images/glaciers.sif python3 -W ignore -m src.slice_n_preprocess -c conf/preprocessing_conf.yaml
 singularity exec --bind $SLURM_TMPDIR /scratch/sankarak/images/glaciers.sif python3 -W ignore -m src.filter_debris_by_perc --data_path $SLURM_TMPDIR/data/sat_data.csv
-singularity exec --bind $SLURM_TMPDIR /scratch/sankarak/images/glaciers.sifpython3 -m src.normalize --base_dir $SLURM_TMPDIR/data
-singularity exec --bind $SLURM_TMPDIR /scratch/sankarak/images/glaciers.sifpython3 -m src.normalize --base_dir $SLURM_TMPDIR/data --data_file sat_data_p_deb.csv --normalization_data norm_p_deb.pkl
+singularity exec --bind $SLURM_TMPDIR /scratch/sankarak/images/glaciers.sif python3 -m src.normalize --base_dir $SLURM_TMPDIR/data
+singularity exec --bind $SLURM_TMPDIR /scratch/sankarak/images/glaciers.sif python3 -m src.normalize --base_dir $SLURM_TMPDIR/data --data_file sat_data_p_deb.csv --normalization_data norm_p_deb.pkl
 
 # tar (compressing takes a huge time) and copy the processed data on $SCRATCH
 cd $SLURM_TMPDIR
 tar -cf processed_glacier_data.tar data
-cp $SLURM_TMPDIR/processed_glacier_data.tar $SCRATCH
-chomd a+r $SCRATCH/processed_glacier_data.tar
+cp $SLURM_TMPDIR/processed_glacier_data.tar $SCRATCH/data
+chmod a+r $SCRATCH/data/processed_glacier_data.tar

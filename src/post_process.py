88458123f1646a5abd7c10723fa410291d81b906
Kris Sankaran <sankaran.kris@gmail.com>
2020-03-11 18:28:53 -0400

    sketched postprocess

--- src/post_process.py
+++ src/post_process.py
@@ -1,48 +1,41 @@
-import pandas as pd
-import numpy as np
-import addict
-import postprocess_funs
+#!/usr/bin/env python
 from addict import Dict
-import pdb
-import glob
-import os
 from argparse import ArgumentParser
+import addict
+from pathlib import Path
+import glob
+import numpy as np
+import os
+import pandas as pd
+import yaml
+import postprocess_funs as pf
 
-if __name__ == '__main__':
-
-    # inputs:
-    #    - directory with sliced images
-    #    - csv with the slices metadata
-    #    - postprocessing specific parameters (e.g., filter percentage, validation method)
-    #
-    # outputs:
-    #    - transformed images in their correct directories
-
+if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-d", "--slice_dir", type=str, help="path to directory with all the slices")
     parser.add_argument("-m", "--slice_meta", type=str, help="path to the slices metadata")
     parser.add_argument("-o", "--output_dir", type=str, default="processed", help="path to output directory for postprocessed files")
-    parser.add_argument("-c", "--conf", type=str, help="Path to the file specifying postprocessing options.")
+    parser.add_argument("-c", "--conf", type=str, default="conf/postprocess.yaml", help="Path to the file specifying postprocessing options.")
     args = parser.parse_args()
 
-    conf = Dict(yaml.load(args.conf))
-    slice_meta = pd.DataFrame(pathlib.Path(args.slice_meta))
+    conf = Dict(yaml.safe_load(open(args.conf, "r")))
+    slice_meta = pd.read_csv(Path(args.slice_meta))
 
     # filter all the slices to the ones that matter
-    keep_ids = filter_directory(
-        args.slices_meta,
+    keep_ids = pf.filter_directory(
+        slice_meta,
         filter_perc=conf.filter_percentage,
         filter_channel=conf.filter_channel
     )
 
     # validation: get ids for the ones that will be training vs. testing.
-    split_fun = getattr(postprocess_funs, args.split_method)
+    split_fun = getattr(pf, args.split_method)
     split_ids = split_fun(keep_ids, split_ratio, slices_meta)
-    target_locs = postprocess_funs.reshuffle(args.slice_dir, split_ids)
+    target_locs = pf.reshuffle(args.slice_dir, split_ids)
 
     # global statistics: get the means and variances in the train split
     train_img_paths = glob.glob(Path(args.output_dir, "train", "*img*"))
-    stats = generate_stats(
+    stats = pf.generate_stats(
         train_img_paths,
         conf.normalization_sample_size,
         path(args.output_dir, "stats_train.json")
@@ -51,8 +44,10 @@ if __name__ == '__main__':
     # postprocess individual images (all the splits)
     for split_type in target_locs:
         for i in range(len(target_locs[split_type])):
-            postprocess(
+            funs_seq = [getattr(pf, f) for f in conf.funs]
+            pf.postprocess(
                 target_locs[split_type][i]["img"],
                 target_locs[split_type][i]["mask"],
-                [normalize, remove_nas, ...]
+                funs_seq,
+                stats=stats
             )

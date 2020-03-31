d034a20a1310c7d4b12b4f7abed2e36a94f7bb21
AryalBibek <baryal@miners.utep.edu>
2020-03-31 13:43:18 -0600

    bibek changes

--- src/eval.py
+++ src/eval.py
@@ -3,6 +3,8 @@ import numpy as np
 import torch
 from src.slice import slice_tile
 import rasterio
+from src.postprocess_funs import postprocess_tile
+import yaml
 
 """
 infer_tile(tile) -> mask
@@ -20,19 +22,28 @@ inputs:
 inference_wrapper(VRT + selected region) -> mask for that region
 """
 
+def merge_patches(files, overlap):
+    """
+    This function is to merge the patches in files with overlap = overlap
+    2*3 != 3*2
+    How to solve this? more information might be needed regarding the size of the final segmentation patch.
+    """
+    pass
+
+
 def infer_tile(img, model, preprocess_conf, slice_opts=None):
     if slice_opts is None:
         slice_opts = {
             "overlap": 0,
             "size": (512, 512)
         }
-
+    # To improve : overlap is hardcoded in src function rather than defining in configuration file.
     process_opts = yaml.load(open(preprocess_conf, "r"))
 
     slice_imgs = slice_tile(img.read(), **slice_opts)
     predictions = []
     for patch in slice_imgs:
-        patch = postprocess_tile(patch, preprocess_conf)
+        patch = postprocess_tile(patch, process_opts)
         predictions.append(model.infer(patch))
 
-    return merge_patches(predictions, overlap)
+    return merge_patches(predictions, process_opts.data.overlap)

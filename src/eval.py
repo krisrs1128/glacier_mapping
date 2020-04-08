41e0e52b9ab0848ecf4d4086d9449b6559b39567
Ubuntu <baryal@bibek-icimod.ga3o0jzfn4keroxrasgvtnu2qg.bx.internal.cloudapp.net>
2020-04-08 16:34:56 +0000

    machine learning codes / bar graph, csv out in eval.py

--- src/eval.py
+++ src/eval.py
@@ -9,6 +9,10 @@ import rasterio
 import torch
 import yaml
 
+import matplotlib 
+import matplotlib.pyplot as plt
+import pandas as pd
+
 
 def merge_patches(patches, overlap, output_size):
     """
@@ -65,11 +69,111 @@ def infer_tile(img, model, process_conf):
 
     return merge_patches(predictions, process_opts.slice.overlap, img_np.shape)
 
+def get_hist(img, mask):
+    """
+    Defined:
+        max number of points in csv for each label(n_points)
+    Input: 
+        raster image, (expected raster image)
+        mask (expected numpy array)
+    Output:
+        pandas dataframe
+        graph
+    """
+    n_points = 1000
+    df = pd.DataFrame(columns=["B1","B2","B3","B4","B5","B6_VCID_1","B6_VCID_2","B7","B8","BQA","elevation","slope","label"])
+
+    # prepare the canvas
+    x = np.arange(0.40, 2.40, 0.01)
+    y = np.arange(0.40, 2.40, 0.01)
+    x_values = np.array((0.49,0.56,0.67,0.84,1.66,2.22))
+    fig, ax = plt.subplots()
+    ax.axvspan(0.45, 0.52, alpha=0.2, color='blue')
+    ax.axvspan(0.52, 0.60, alpha=0.2, color='green')
+    ax.axvspan(0.63, 0.69, alpha=0.2, color='red')
+    ax.axvspan(0.77, 0.90, alpha=0.2, color='grey')
+    ax.axvspan(1.55, 1.75, alpha=0.2, color='grey')
+    ax.axvspan(2.08, 2.35, alpha=0.2, color='grey')
+    ax.set_xticks(x_values, minor=False)
+    ax.set_xticklabels(["B1","B2","B3","B4","B5","B6_VCID_2"], fontdict=None, minor=False)
+    plt.xlabel("Wavelength μm")
+    plt.ylabel("Intensity")
+
+    # read image
+    img_np = img.read()
+    img_np = np.transpose(img_np, (1, 2, 0))
+
+    # img_np = img
+
+    clean_index = np.argwhere(mask == 0)
+    debris_index = np.argwhere(mask == 1)
+    background_index = np.argwhere(mask == 2)
+    np.random.shuffle(clean_index)
+    np.random.shuffle(debris_index)
+    np.random.shuffle(background_index)
+
+    clean_index = clean_index[:n_points].tolist()
+    debris_index = debris_index[:n_points].tolist()
+    background_index = background_index[:n_points].tolist()
+
+
+    clean_value = []
+    debris_value = []
+    background_value = []
+    for index in clean_index:
+        clean_value.append(img_np[index[0],index[1],:])
+        row = pd.Series(list(np.append(img_np[index[0],index[1],:], "Clean Ice")), index=df.columns)
+        df = df.append(row, ignore_index=True)
+    for index in debris_index:
+        debris_value.append(img_np[index[0],index[1],:])
+        row = pd.Series(list(np.append(img_np[index[0],index[1],:], "Debris")), index=df.columns)
+        df = df.append(row, ignore_index=True)
+    for index in background_index:
+        background_value.append(img_np[index[0],index[1],:])
+        row = pd.Series(list(np.append(img_np[index[0],index[1],:], "Background")), index=df.columns)
+        df = df.append(row, ignore_index=True)
+
+    clean_value = np.asarray(clean_value)
+    clean_mean = clean_value.mean(axis=0)
+    clean_std = clean_value.std(axis=0)
+    clean_mean = np.append(clean_mean[0:5],clean_mean[5])
+    debris_value = np.asarray(debris_value)
+    debris_mean = debris_value.mean(axis=0)
+    debris_std = debris_value.std(axis=0)
+    debris_mean = np.append(debris_mean[0:5],debris_mean[5])
+    background_value = np.asarray(background_value)
+    background_mean = background_value.mean(axis=0)
+    background_std = background_value.std(axis=0)
+    background_mean = np.append(background_mean[0:5],background_mean[5])
+            
+    for (x,y) in zip(x_values,clean_mean):
+        ax.plot(x, y, 'bo')
+
+    for (x,y) in zip(x_values,debris_mean):
+        ax.plot(x, y, 'ro')
+
+    for (x,y) in zip(x_values,background_mean):
+        ax.plot(x, y, 'go')
+
+    plt.plot(x_values, clean_mean, color='blue', label="Clean Glaciers")
+    plt.plot(x_values, debris_mean, color='red', label="Debris Glaciers")
+    plt.plot(x_values, background_mean, color='green', label="Background")
+    plt.title("Wavelength vs Normalized intensity")
+    ax.legend(bbox_to_anchor=(0.65, 1), loc='upper left', borderaxespad=0.)
+
+    # plt.savefig("./test.png")
+    # df.to_csv("./test.csv", index=False)
+    return df, plt
+
+
 if __name__ == '__main__':
     img = rasterio.open("/scratch/sankarak/data/glaciers/img_data/2005/hkh/LE07_140041_20051012.tif")
+    # img = np.load("./shared/true_image.npy")
+    # mask = np.load("./shared/true_label.npy")
     process_conf = "//home/sankarak/glacier_mapping/conf/postprocess.yaml"
     model_path = "/scratch/sankarak/data/glaciers/model_188.pt"
 
+    df, plt = get_hist(img, mask)
     state_dict = torch.load(model_path, map_location="cpu")
     model = Unet(10, 1, 4)
     model.load_state_dict(state_dict)

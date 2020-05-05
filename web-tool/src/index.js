5b10038ccbeb6c779b57fbb86670b19efbf2bbfd
Kris Sankaran <sankaran.kris@gmail.com>
2020-05-04 23:40:01 -0700

    example post request

--- web-tool/src/index.js
+++ web-tool/src/index.js
@@ -1,22 +1,39 @@
-import * as d3s from 'd3-selection';
-import * as d3sm from 'd3-selection-multi';
+import './map.css';
 import 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import dataset from '../conf/dataset.json';
-import { state } from './globals';
-import './map.css';
+import * as d3f from 'd3-fetch';
+import * as d3s from 'd3-selection';
+import * as d3sm from 'd3-selection-multi';
 import * as f from './funs';
+import dataset from '../../conf/dataset.json';
+import { state, map } from './globals';
 
-/* Setup the map */
+// Setup the map
 f.initializeMap("#root");
 f.addButtons("#controls")
+map.on("zoom", f.redraw)
 
-// on click on button, add new empty polygson.
-// this polygon becomes the focus
+// when the user presses shift, highlight a region on which to draw predictions
 
-// on click on map, the focus polygon gets a new node
-//
+// when the user clicks, make a post to the predictions
+d3s.select("#root")
+  .append("button")
+  .text("predict")
+  .on("click", predFun);
 
+function predFun() {
+  d3f.json("http://localhost:4446/predPatch", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      "extent": [[0, 1], [2, 3]],
+      "dataset": dataset,
+      "classes": dataset["classes"]
+    })
+  }).then((data) => console.log(data));
+}
+
+// extract the geojson associated with a prediction
 
 
 // This is needed for Hot Module Replacement

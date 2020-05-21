3d1cefbf2a9de03ab9d51a490ad49a1410d99baa
Kris Sankaran <sankaran.kris@gmail.com>
2020-05-21 10:50:34 -0700

    add layer control

--- web-tool/src/globals.js
+++ web-tool/src/globals.js
@@ -1,6 +1,7 @@
 import 'leaflet';
 import * as d3s from 'd3-selection';
-import dataset from '../../conf/dataset.json';
+import dataset from '../../conf/dataset';
+import layerInfo from '../../conf/layerInfo';
 
 export const state = {
   polygons: [],
@@ -22,12 +23,26 @@ d3s.select("#map")
   .style("height", "500px")
   .style("width", "700px");
 
+let tiles = {
+  "ESRI": L.tileLayer(
+    layerInfo.ESRI.url,
+    {attribution: ""}
+  ),
+  "2-4-5": L.tileLayer(
+    dataset.basemapLayer.url,
+    {tms: true}
+  ),
+};
+
 export let map = L.map("map", {
-  zoomControl: false,
+  zoomControl: true,
   crs: L.CRS.EPSG3857, // this is the projection CRS (EPSG:3857), but it is different than the data CRS (EPSG:4326). See https://gis.stackexchange.com/questions/225765/leaflet-map-crs-is-3857-but-coordinates-4326/225786.
   center: dataset.basemapLayer.initialLocation,
   zoom: dataset.basemapLayer.initialZoom,
-  url: dataset.basemapLayer.url,
+  layers: Object.values(tiles)
 });
+
+
+L.control.layers(tiles).addTo(map);
 
 export let backendUrl = "http://localhost:4446/";

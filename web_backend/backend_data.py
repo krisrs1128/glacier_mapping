cf209b8d2981b327a5196ace7e23e055844b2065
Kris Sankaran <sankaran.kris@gmail.com>
2020-06-08 19:23:10 -0700

    need to reproject

--- web_backend/backend_data.py
+++ web_backend/backend_data.py
@@ -4,16 +4,31 @@ Utilities for Managing VRTs
 
 2020-05-05 18:11:08
 """
-import numpy as np
-import argparse
-import pandas as pd
-import fiona
-import shapely.geometry
-import pathlib
 from osgeo import gdal
+import argparse
+import fiona
+import numpy as np
+import pandas as pd
+import pathlib
+import glob
+import rasterio
+import shapely.geometry
+import subprocess
+
+
+def reproject_directory(input_dir, output_dir, dst_epsg=4326):
+    inputs = pathlib.Path(input_dir).glob("*.tif")
+    for im_path in inputs:
+        print(f"reprojecting {str(im_path)}")
+        im = rasterio.open(im_path)
+        output_path = pathlib.Path(output_dir, f"{im_path.stem}-warped.tif")
+        subprocess.call(["gdalwarp", "-s_srs", str(im.crs), "-t_srs",
+                         f"EPSG:{dst_epsg}", str(im_path),
+                         "-wo", "NUM_THREADS=ALL_CPUS", str(output_path)])
+
 
 def vrt_from_dir(input_dir, output_path="./output.vrt", **kwargs):
-    inputs = pathlib.Path(input_dir).glob("**/*.tif*")
+    inputs = glob.glob(f"{input_dir}*.tif")
     vrt_opts = gdal.BuildVRTOptions(**kwargs)
     gdal.BuildVRT(output_path, inputs, options=vrt_opts)
 
@@ -33,7 +48,8 @@ if __name__ == "__main__":
     parser.add_argument("-t", "--tile", default=False)
     args = parser.parse_args()
 
+    reproject_directory(args.input_dir, args.output_dir)
     vrt_path = pathlib.Path(args.output_dir, args.output_name)
-    vrt_from_dir(args.input_dir, vrt_path)
+    vrt_from_dir(args.input_dir, str(vrt_path))
     if args.tile:
         tiles(vrt_path, args.output_dir)

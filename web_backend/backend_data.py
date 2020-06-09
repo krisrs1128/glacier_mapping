7a9fec1f7bbdb06a8a8cf868eecb58ea01e90fa7
Kris Sankaran <sankaran.kris@gmail.com>
2020-06-08 20:13:52 -0700

    run pylint
    5.5 -> 8.6!

--- web_backend/backend_data.py
+++ web_backend/backend_data.py
@@ -4,41 +4,52 @@ Utilities for Managing VRTs
 
 2020-05-05 18:11:08
 """
-from osgeo import gdal
-import argparse
-import fiona
-import gdal2tiles
 import glob
-import numpy as np
-import pandas as pd
 import pathlib
-import rasterio
-import shapely.geometry
 import subprocess
+import argparse
+import gdal2tiles
+import rasterio
+from osgeo import gdal
 
 
 def reproject_directory(input_dir, output_dir, dst_epsg=4326):
+    """
+    Warp all Tiffs from one directory to 4326
+    """
     inputs = pathlib.Path(input_dir).glob("*.tif")
     for im_path in inputs:
         print(f"reprojecting {str(im_path)}")
-        im = rasterio.open(im_path)
+        loaded_im = rasterio.open(im_path)
         output_path = pathlib.Path(output_dir, f"{im_path.stem}-warped.tif")
-        subprocess.call(["gdalwarp", "-s_srs", str(im.crs), "-t_srs",
+        subprocess.call(["gdalwarp", "-s_srs", str(loaded_im.crs), "-t_srs",
                          f"EPSG:{dst_epsg}", str(im_path),
                          "-wo", "NUM_THREADS=ALL_CPUS", str(output_path)])
 
 
 def vrt_from_dir(input_dir, output_path="./output.vrt", **kwargs):
+    """
+    Build a VRT Indexing all Tiffs in a directory
+    """
     inputs = glob.glob(f"{input_dir}*.tif")
     vrt_opts = gdal.BuildVRTOptions(**kwargs)
     gdal.BuildVRT(output_path, inputs, options=vrt_opts)
 
 
 def tiles(input_vrt, output_dir, zoom_levels="10"):
+    """
+    Generate PNG tiles from a VRT
+    """
     path = pathlib.Path(input_vrt)
     intermediate = f"{path.parent}/{path.resolve().stem}-byte.vrt"
     subprocess.call(["gdal_translate", "-ot", "Byte", input_vrt, f'{intermediate}'])
-    gdal2tiles.generate_tiles(f"{intermediate}", output_dir, zoom=zoom_levels, verbose=True, tile_size=1056)
+    gdal2tiles.generate_tiles(
+        f"{intermediate}",
+        output_dir,
+        zoom=zoom_levels,
+        verbose=True,
+        tile_size=1056
+    )
 
 
 if __name__ == "__main__":

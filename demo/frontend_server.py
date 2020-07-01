44ff30163d1ff6a1936dade98dc9ac5ee60ceb76
Kris Sankaran <sankaran.kris@gmail.com>
2020-07-01 10:26:12 -0700

    missed change in frontend_server

--- demo/frontend_server.py
+++ demo/frontend_server.py
@@ -16,7 +16,7 @@ def root_app():
 
 def main():
     parser = argparse.ArgumentParser(description="Frontend Server")
-    parser.add_argument("--host", action="store", dest="host", type=str, help="Host to bind to", default="localhost")
+    parser.add_argument("--host", action="store", dest="host", type=str, help="Host to bind to", default="0.0.0.0")
     parser.add_argument("--port", action="store", dest="port", type=int, help="Port to listen on", default="4040")
     args = parser.parse_args(sys.argv[1:])
 

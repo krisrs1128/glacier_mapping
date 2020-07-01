5736f961500df62e65c1c18e1d30fdb802fafac3
Kris Sankaran <sankaran.kris@gmail.com>
2020-07-01 10:12:27 -0700

    make sure to include frontend server in mini example

--- demo/server.py
+++ demo/server.py
@@ -1,10 +1,8 @@
 #! /usr/bin/env python3
-# -*- coding: utf-8 -*-
-# vim:fenc=utf-8
-# pylint: disable=E1137,E1136,E0110,E1101
 import beaker.middleware
 import bottle
 import cheroot.wsgi
+import json
 app = bottle.Bottle()
 
 #---------------------------------------------------------------------------------------
@@ -22,7 +20,7 @@ def enable_cors():
     This globally enables Cross-Origin Resource Sharing (CORS) headers for every response from this server.
     '''
     print("after request")
-    bottle.response.headers['Access-Control-Allow-Origin'] = 'http://52.247.203.144:4040'
+    bottle.response.headers['Access-Control-Allow-Origin'] = '*'
     bottle.response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
     bottle.response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
@@ -30,9 +28,7 @@ def enable_cors():
 @app.post("/test")
 def test():
     bottle.response.content_type = 'application/json'
-    data = bottle.response.json
     print("this is just a test")
-    bottle.response.status = 200
-    return json.dumps(data)
+    return json.dumps({"test": "test"})
 
 bottle.run(app, host="localhost", port="8080")

47d7a8f6441eefe8649ca10eb1bbefd5bf651ccd
Kris Sankaran <sankaran.kris@gmail.com>
2020-07-02 21:05:11 -0700

    we need to export models.js

---
+++ conf/models.js
@@ -0,0 +1,41 @@
+export default {
+  "shimas_unet": {
+    "metadata": {
+      "displayName": "UNet Shima Kind Durian 140"
+    },
+    "model": {
+      "type": "pytorch",
+      "numParameters": 7790949,
+      "inputShape": [
+        512,
+        512,
+        10
+      ],
+      "fn": "../data/models/model_188.pt",
+      "fineTuneLayer": 0
+    }
+  },
+  "benjamins_unet": {
+    "metadata": {
+      "displayName": "Benjamins model"
+    },
+    "model": {
+      "type": "pytorch",
+      "numParameters": null,
+      "args": {
+        "inchannels": 3,
+        "outchannels": 1,
+        "net_depth": 5,
+        "channel_layer": 16
+      },
+      "inputShape": [
+        512,
+        512,
+        3
+      ],
+      "fn": "/mnt/blobfuse/glaciers/runs/minimal_run/models/model_5.pt",
+      "fineTuneLayer": 0,
+      "process": "conf/postprocess.yaml"
+    }
+  }
+}

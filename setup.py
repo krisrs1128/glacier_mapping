b434357f319ef62cac8fe8ed3d8bcd1df2baf829
Kris Sankaran <sankaran.kris@gmail.com>
2020-07-30 21:11:08 -0500

    create structure that allows installation from glacier_mapping dir

--- setup.py
+++ setup.py
@@ -1,22 +1,27 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open('glacier_mapping/requirements.txt') as f:
+    required = f.read().splitlines()
 
 setuptools.setup(
-    name="glacier-mapping-krisrs1128", # Replace with your own username
-    version="0.0.1",
-    author="Various",
-    author_email="misc",
-    description="A small example package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
+    name='glacier_mapping',
+    version='0.1.0',
+    description='A example Python package',
+    url='https://github.com/shuds13/pyexample',
+    author='Stephen Hudson',
+    author_email='shudson@anl.gov',
+    license='BSD 2-clause',
+    packages=['glacier_mapping'],
+    install_requires=required,
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
-    python_requires='>=3.6',
 )

2f872934eb99c5492b83ed6de7ea82df3f42034e
Kris Sankaran <sankaran.kris@gmail.com>
2020-07-31 17:51:45 -0500

    return to previous classifiers

--- setup.py
+++ setup.py
@@ -18,8 +18,10 @@ setuptools.setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
         'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
 )

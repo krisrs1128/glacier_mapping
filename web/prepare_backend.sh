61c1b87b5407cbff3a8aab47130f02c1e14020ba
Kris Sankaran <sankaran.kris@gmail.com>
2020-09-05 17:36:57 -0500

    try fixing some installation issues, and change datadirs

--- web/prepare_backend.sh
+++ web/prepare_backend.sh
@@ -11,9 +11,9 @@ source .env
 # mkdir -p $ROOT_DIR/web/outputs/tiles/
 
 # data prep for backend
-python3 -m web.backend.backend_data -d $DATA_DIR/raw/img_data/mini/ -o $ROOT_DIR/web/frontend/outputs/tiles/ -n output-full.vrt # tiles/ directory must exist
-python3 -m web.backend.backend_data -d $DATA_DIR/raw/img_data/mini/ -o $ROOT_DIR/web/frontend/outputs/tiles/ -n output-full.vrt # tiles/ directory must exist
-python3 -m web.backend.backend_data -d $DATA_DIR/raw/img_data/mini/ -o $ROOT_DIR/web/frontend/outputs/tiles/ -n output-245.vrt --tile True --bandList 5 4 2
+python3 -m web.backend.backend_data -d $DATA_DIR/img_data/ -o $ROOT_DIR/web/frontend/outputs/tiles/ -n output-full.vrt # tiles/ directory must exist
+python3 -m web.backend.backend_data -d $DATA_DIR/img_data/ -o $ROOT_DIR/web/frontend/outputs/tiles/ -n output-full.vrt # tiles/ directory must exist
+python3 -m web.backend.backend_data -d $DATA_DIR/img_data/ -o $ROOT_DIR/web/frontend/outputs/tiles/ -n output-245.vrt --tile True --bandList 5 4 2
 python3 -m web.backend.backend_data -d $DATA_DIR/processed/preds/ -o $ROOT_DIR/web/frontend/outputs/pred_tiles/ -n y_hat.vrt --bandList 1 --tile True # tiles/ directory must exist
 
 # copy tile outputs to $ROOT_DIR/web/outputs/

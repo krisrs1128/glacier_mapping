92408b385c060a07695a80f1ddfef8572b14a2b4
Kris Sankaran <sankaran.kris@gmail.com>
2020-09-19 13:43:04 -0500

    missing for loop do

--- scripts/geo/setup_data.sh
+++ scripts/geo/setup_data.sh
@@ -23,6 +23,8 @@ mkdir $split_dir/1/
 python3 -m glacier_mapping.experiment_helpers.geo -d $input_dir -o $split_dir/1/ -r True
 
 for i in $( seq 2 $n_folds)
+do
+    echo $i
     mkdir $split_dir/$i
     python3 -m glacier_mapping.experiment_helpers.geo -d $split_dir/1/ -o $split_dir/$i/
 done
@@ -32,6 +34,7 @@ python3 -m scripts.make_slices -m $mask_path -o $DATA_DIR/expers/geographic/
 
 # construct different folds
 for i in $( seq 1 $n_folds)
+do
     python3 -m scripts.geo.conf -i $i -t conf/geo/postprocess.yaml -o $split_dir/$i/postprocess.yaml
     python3 -m scripts.process_slices -o $split_dir/$i -m $DATA_DIR/expers/geographic/slices/slices.geojson -p $split_dir/$i/postprocess.yaml
 done;

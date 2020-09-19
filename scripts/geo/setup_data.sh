2624e51494ff221254bb8c33dd386dc903a1df35
Kris Sankaran <sankaran.kris@gmail.com>
2020-09-18 22:10:02 -0500

    try on everything?

--- scripts/geo/setup_data.sh
+++ scripts/geo/setup_data.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 cd $ROOT_DIR
 export split_dir=$DATA_DIR/expers/geographic/splits/
+export input_dir=$DATA_DIR/analysis_images/
 rm -rf $split_dir
 mkdir -p $split_dir
 
@@ -8,7 +9,7 @@ mkdir -p $split_dir
 mkdir $split_dir/1/
 python3 -m glacier_mapping.experiment_helpers.geo -d $DATA_DIR/expers/geographic/test_input/ -o $split_dir/1/ -r True
 
-for i in {2..3}; do
+for i in {2..10}; do
     echo $i
     mkdir $split_dir/$i
     python3 -m glacier_mapping.experiment_helpers.geo -d $split_dir/1/ -o $split_dir/$i/
@@ -18,7 +19,7 @@ done
 python3 -m scripts.make_slices -m conf/geo/mask.yaml -o $DATA_DIR/expers/geographic/
 
 # construct different folds
-for i in {1..3}; do
+for i in {1..10}; do
     python3 -m scripts.geo.conf -i $i -t conf/geo/postprocess.yaml -o $split_dir/$i/postprocess.yaml
     python3 -m scripts.process_slices -o $split_dir/$i -m $DATA_DIR/expers/geographic/slices/slices.geojson -p $split_dir/$i/postprocess.yaml
 done;

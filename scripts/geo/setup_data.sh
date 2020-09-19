09628beb13cafdf89962ccd6772ef600288ce275
Kris Sankaran <sankaran.kris@gmail.com>
2020-09-18 20:58:14 -0500

    brackets in loop

--- scripts/geo/setup_data.sh
+++ scripts/geo/setup_data.sh
@@ -7,7 +7,7 @@ mkdir -p $split_dir
 mkdir $split_dir/1/
 python3 -m glacier_mapping.experiment_helpers.geo -d $DATA_DIR/expers/geographic/test_input/ -o $split_dir/1/ -r True
 
-for i in 2 .. 10; do
+for i in {2 .. 10}; do
     echo $i
     mkdir $split_dir/$i
     python3 -m glacier_mapping.experiment_helpers.geo -d $split_dir/1/ -o $split_dir/$i/
@@ -17,7 +17,7 @@ done
 python3 -m scripts.make_slices -m conf/geo/mask.yaml -o $DATA_DIR/expers/geographic/
 
 # construct different folds
-for i in 1 .. 10; do
+for i in {1 .. 10}; do
     python3 -m scripts.geo.conf -i $i -t conf/geo/postprocess.yaml -o $split_dir/postprocess.yaml
     python3 -m scripts.process_slices -o $split_dir/$i -p conf/geo/process_$i.yaml
 done;

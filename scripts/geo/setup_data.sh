3561ac5cf72fb4b9eb3e4f3f8459b939b6d4c007
Kris Sankaran <sankaran.kris@gmail.com>
2020-09-19 13:13:36 -0500

    test on a small mask, for geo experiments

--- scripts/geo/setup_data.sh
+++ scripts/geo/setup_data.sh
@@ -5,8 +5,12 @@ source /home/kris/glacier_mapping/.env
 if [ $1 == "test" ];
 then
     export input_dir=$DATA_DIR/expers/geographic/test_input/
+    export n_folds=3
+    export mask_path=conf/geo/mask_small.yaml
 else
     export input_dir=$DATA_DIR/analysis_images
+    export n_folds=10
+    export mask_path=conf/geo/mask.yaml
 fi
 
 cd $ROOT_DIR
@@ -18,17 +22,17 @@ mkdir -p $split_dir
 mkdir $split_dir/1/
 python3 -m glacier_mapping.experiment_helpers.geo -d $input_dir -o $split_dir/1/ -r True
 
-for i in {2..10}; do
+for i in {2..$n_folds}; do
     echo $i
     mkdir $split_dir/$i
     python3 -m glacier_mapping.experiment_helpers.geo -d $split_dir/1/ -o $split_dir/$i/
 done
 
 # slice
-python3 -m scripts.make_slices -m conf/geo/mask.yaml -o $DATA_DIR/expers/geographic/
+python3 -m scripts.make_slices -m $mask_path -o $DATA_DIR/expers/geographic/
 
 # construct different folds
-for i in {1..10}; do
+for i in {1..$n_folds}; do
     python3 -m scripts.geo.conf -i $i -t conf/geo/postprocess.yaml -o $split_dir/$i/postprocess.yaml
     python3 -m scripts.process_slices -o $split_dir/$i -m $DATA_DIR/expers/geographic/slices/slices.geojson -p $split_dir/$i/postprocess.yaml
 done;

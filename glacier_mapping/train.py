94f73189a3fe2959d91e45b53c0bdd8b1c63b528
Shimaa <shimaa.baraka@gmail.com>
2020-09-23 14:19:19 +0200

    logging images

--- glacier_mapping/train.py
+++ glacier_mapping/train.py
@@ -149,7 +149,10 @@ def log_images(writer, frame, batch, epoch, stage="train"):
     squash = lambda x: (x - x.min()) / (x.max() - x.min())
 
     x, y = batch
-    y_hat = frame.predict(frame.infer(x))
+    if frame.multi_class:
+        y_hat = torch.nn.Softmax(1)(frame.infer(x))
+    else:
+        y_hat = torch.sigmoid(frame.infer(x))
     y = torch.flatten(y.permute(0, 1, 3, 2), start_dim=2)
     y_hat = torch.flatten(y_hat.permute(0, 1, 3, 2), start_dim=2)
     if epoch == 0:

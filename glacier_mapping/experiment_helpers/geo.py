489ebe7ccb6efb617b2118c21d198a23cc61d38a
Kris Sankaran <sankaran.kris@gmail.com>
2020-09-26 17:07:37 -0500

    work_df -> work_region

--- glacier_mapping/experiment_helpers/geo.py
+++ glacier_mapping/experiment_helpers/geo.py
@@ -80,9 +80,8 @@ if __name__ == "__main__":
     train, test = geo_split(work_region["geometry"][0])
 
     # convert to geopandas df, and svae to geojson
-    work_df = create_gdf(work_region)
     train_df = create_gdf(train)
     test_df = create_gdf(test)
-    work_df.to_file(pathlib.Path(args.output_dir) / "work_region.geojson", driver="GeoJSON")
+    work_region.to_file(pathlib.Path(args.output_dir) / "work_region.geojson", driver="GeoJSON")
     train_df.to_file(pathlib.Path(args.output_dir) / "train.geojson", driver="GeoJSON")
     test_df.to_file(pathlib.Path(args.output_dir) / "test.geojson", driver="GeoJSON")

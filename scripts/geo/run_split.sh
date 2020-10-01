6458de4aaad198268314b4e277fa91beb395f107
Kris Sankaran <sankaran.kris@gmail.com>
2020-10-01 14:11:57 -0500

    update requirements, fix path to scripts

--- scripts/geo/run_split.sh
+++ scripts/geo/run_split.sh
@@ -5,5 +5,5 @@ git clone https://github.com/krisrs1128/glacier_mapping.git
 cd glacier_mapping
 source .env
 
-cd glacier_mapping/scripts
+cd scripts
 python3 train.py -d /home/ksankaran/splits/1/ -c ../conf/train.yaml  -p /home/ksankaran/splits/1/postprocess.yaml -r geo

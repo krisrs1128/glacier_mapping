76621de68926102ff197e9da9d017edd432e3b91
Kris Sankaran <sankaran.kris@gmail.com>
2020-10-02 23:43:04 -0500

    reinstall on each job

--- scripts/geo/run_split.sh
+++ scripts/geo/run_split.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 tar -zxvf splits.tar.gz
+pip3 install git+https://github.com/krisrs1128/glacier_mapping.git
 
 git clone https://github.com/krisrs1128/glacier_mapping.git
 cd glacier_mapping

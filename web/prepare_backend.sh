200dab8367aed3a73a801c54b36204fc0cb94352
Kris Sankaran <sankaran.kris@gmail.com>
2020-11-02 17:30:19 -0600

    read from unique_tiles, and back off to 10 processes

--- web/prepare_backend.sh
+++ web/prepare_backend.sh
@@ -4,19 +4,18 @@
 source .env
 
 # if data or directories don't exist, you'll need to fill them in
-# mkdir -p $DATA_DIR/web/basemap/
-mkdir -p $ROOT_DIR/web/basemap/
+mkdir -p $DATA_DIR/web/basemap/
 
 # data prep for backend
-python3 -m web.backend.backend_data -d $DATA_DIR/web_data/img_data/ -o $DATA_DIR/web/basemap/ -n output-full.vrt;
-python3 -m web.backend.backend_data -d $DATA_DIR/web_data/img_data/ -o $DATA_DIR/web/basemap/ -n output-245.vrt --bandList 5 4 2
+python3 -m web.backend.backend_data -d $DATA_DIR/unique_tiles/ -o $DATA_DIR/web/basemap/ -n output-full.vrt;
+python3 -m web.backend.backend_data -d $DATA_DIR/unique_tiles/ -o $DATA_DIR/web/basemap/ -n output-245.vrt --bandList 5 4 2
 
 cd $DATA_DIR/web/basemap/
 gdal_translate -ot Byte output-245.vrt output-245-byte.vrt
 
 for i in $( seq 8 14 )
 do
-    gdal2tiles.py -z $i --processes 25 output-245-byte.vrt .
+    gdal2tiles.py -z $i --processes 10 output-245-byte.vrt .
     cp -r $i $ROOT_DIR/web/basemap/
 done;
 
